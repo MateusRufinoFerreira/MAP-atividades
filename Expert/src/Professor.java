0131400f63c747ca91a9f93a69d1d4f5c97d192b
Mateus Rufino Ferreira <163481727+MateusRufinoFerreira@users.noreply.github.com>
2025-03-21 19:18:16 -0300

    Add files via upload

---
+++ Expert/src/Professor.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Professor {
+    private String nome;
+    private String horario;
+    private List<Disciplina> disciplinas = new ArrayList<>();
+
+    public Professor(String nome, String horario) {
+        this.nome = nome;
+        this.horario = horario;
+    }
+
+    public void adicionarDisciplina(Disciplina d) {
+        disciplinas.add(d);
+    }
+
+    public List<Disciplina> getDisciplinas() {
+        return disciplinas;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+    
+    public String getHorario() {
+        return horario;
+    }
+}
