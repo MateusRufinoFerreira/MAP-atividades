556ab5076d8ae8ab76d5aaf561c694e20b01e62c
Mateus Rufino Ferreira <163481727+MateusRufinoFerreira@users.noreply.github.com>
2025-03-27 17:22:20 -0300

    Update Instituicao.java

--- Creator/main/java/org/example/Instituicao.java
+++ Creator/main/java/org/example/Instituicao.java
@@ -30,33 +30,3 @@ public class Instituicao {
     }
 
 }
-========
-public class Instituicao {
-    private String nome;
-    private ControleAcademico controleAcademico;
-    private RDM rdm;
-
-    public Instituicao(String nome) {
-        this.nome = nome;
-        this.controleAcademico = new ControleAcademico();
-        this.rdm = new RDM();
-    }
-
-    public void adicionarTurma(Turma turma) {
-        controleAcademico.adicionarTurma(turma);
-    }
-
-    public void matricularAluno(Aluno aluno, Turma turma) {
-        controleAcademico.matricularAlunoEmTurma(aluno, turma);
-        rdm.registrarMatricula(aluno, turma.getDisciplina());
-    }
-
-    public void listarDisciplinasDeAluno(Aluno aluno) {
-        rdm.listarDisciplinasDoAluno(aluno);
-    }
-    
-    public List<Turma> listarTurmas() {
-        return controleAcademico.getTurmas();
-    }
-}
->>>>>>>> 604a88ab3d7bcd8fd14e51b1607180fe5b15cff2:Creator/Instituicao.java
